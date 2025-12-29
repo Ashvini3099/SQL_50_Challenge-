9920f8b90a6a5881051b3ead1c861f149487a931
Ashvini Shivraj  Shinde <157118010+Ashvini3099@users.noreply.github.com>
2025-12-29 15:13:13 +0530

    Add files via upload

---
+++ SQLQuery21.sql
@@ -0,0 +1,28 @@
+create table emp_2020
+(
+emp_id int,
+designation varchar(20)
+);
+
+create table emp_2021
+(
+emp_id int,
+designation varchar(20)
+)
+
+insert into emp_2020 values (1,'Trainee'), (2,'Developer'),(3,'Senior Developer'),(4,'Manager');
+insert into emp_2021 values (1,'Developer'), (2,'Developer'),(3,'Manager'),(5,'Trainee');
+
+--- Find the change in employee status  --- by using full pouter join 
+select * from emp_2020 
+select * from emp_2021 
+
+	select isnull(e20.emp_id,e21.emp_id)as emp_id ,e20.designation,e21.designation,
+	case when e20.designation != e21.designation then 'promoted ' 
+	when e21.designation is null then 'resigned'
+	else 'New ' end as comment 
+	from emp_2020 e20 full outer join emp_2021 as e21 
+	on e20 .emp_id = e21.emp_id 
+	where isnull(e20.designation , 'xxx') != isnull(e21. designation , 'yyy') 
+
+	
